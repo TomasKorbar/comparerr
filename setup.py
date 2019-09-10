a66e06c62721be7b2b2f0cc1dfa4b1f59765ca2b
Tomas Korbar <tkorbar@redhat.com>
2019-09-10 13:47:46 +0200

    Version 0.2.0
    Comparerr now has only one executable file which provides more
    comfortable controlling.
    
    Code has been documented.

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup(
     name="comparerr",
-    version="0.1",
+    version="0.2",
     author="Tomas Korbar",
     author_email="tomas.korb@seznam.cz",
     license="GPL3",
     description="Comparerr is a set of scripts allowing you to compare outputs"
     "of pylint and find errors which have been fixed or created.",
     url="https://github.com/TomasKorbar/comparerr",
-    scripts=["bin/comparerr", "bin/pylint2comparerr"],
+    scripts=["bin/comparerr"],
+    packages=["comparerr", "comparerr.utils"],
+    install_requires=["GitPython", "pylint"]
 )
